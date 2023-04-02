1873c5b0d246905cfe1bba57b3d888e208a988a6
CaveNightingale <cavenightingale@foxmail.com>
2023-04-02 20:36:50 +0800

    Let gradlew be executable

--- src/main/java/net/lionarius/skinrestorer/mixin/PlayerCommandMixin.java
+++ src/main/java/net/lionarius/skinrestorer/mixin/PlayerCommandMixin.java
@@ -1,2 +1,20 @@
-package net.lionarius.skinrestorer.mixin;public class PlayerCommandMixin {
+package net.lionarius.skinrestorer.mixin;
+
+import carpet.commands.PlayerCommand;
+import com.mojang.authlib.GameProfile;
+import net.lionarius.skinrestorer.SkinRestorer;
+import net.minecraft.util.UserCache;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Optional;
+
+@Mixin(PlayerCommand.class)
+public class PlayerCommandMixin {
+    @Redirect(method = "cantSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/UserCache;findByName(Ljava/lang/String;)Ljava/util/Optional;"))
+    private static Optional<GameProfile> skinrestorer_skipProfileLoading(UserCache instance, String name) {
+        SkinRestorer.LOGGER.info("Skip profile loading for bot {}", name);
+        return Optional.empty();
+    }
 }
